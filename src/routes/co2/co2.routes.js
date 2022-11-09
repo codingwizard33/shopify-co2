ac2e08cd9195d4578118a840f14111088bb3d599
codingwizard33 <codingwizard33@gmail.com>
2022-11-09 17:10:48 +0400

    update

--- src/routes/co2/co2.routes.js
+++ src/routes/co2/co2.routes.js
@@ -1,14 +1,11 @@
 import express from "express";
-import productCo2Fetch from "../../controller/productCo2Fetch";
-import productsCo2Fetch from "../../controller/productsCo2Fetch";
+import productCo2Fetch from "../../controller/productCo2Fetch.js";
+import productsCo2Fetch from "../../controller/productsCo2Fetch.js";
 
 const co2Router = express.Router();
-
-
 
 co2Router.post("/product", productCo2Fetch);
 
 co2Router.post("/products", productsCo2Fetch);
-
 
 export default co2Router;
